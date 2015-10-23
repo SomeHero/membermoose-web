e62abf7e9742f4bb2ba3d7d9678598cc0131a7b7
James Rhodes <jrhodes@weddingwire.com>
2015-10-23 04:04:26 -0400

    push migration change

--- db/migrate/20151021182038_change_subscription_status_to_int.rb
+++ db/migrate/20151021182038_change_subscription_status_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeSubscriptionStatusToInt < ActiveRecord::Migration
   def change
-    change_column :subscriptions, :status, default: 0, 'integer USING 0'
+    change_column :subscriptions, :status, default: 0
   end
 end
