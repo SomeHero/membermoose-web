99a1d7dff275ae5fe90608595c61bfe81c8427c3
James Rhodes <jrhodes@weddingwire.com>
2015-10-23 04:14:58 -0400

    fix migration again

--- db/migrate/20151021182038_change_subscription_status_to_int.rb
+++ db/migrate/20151021182038_change_subscription_status_to_int.rb
@@ -1,5 +1,5 @@
 class ChangeSubscriptionStatusToInt < ActiveRecord::Migration
   def change
-    change_column :subscriptions, :status, default: 0
+    change_column :subscriptions, :status, :integer, :default => 0
   end
 end
