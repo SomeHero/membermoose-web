99dbcb9771254d6eaaea304bc5b627d7f2f0045e
James Rhodes <jrhodes@weddingwire.com>
2015-11-28 14:28:13 -0500

    retreive plans limit to 100

--- app/services/get_plans.rb
+++ app/services/get_plans.rb
@@ -4,7 +4,7 @@ class GetPlans
     begin
       Stripe.api_key =  stripe_secret_key
 
-      plans = Stripe::Plan.all()
+      plans = Stripe::Plan.all(:limit => 100)
     rescue Stripe::StripeError => e
       return e.message
     end
